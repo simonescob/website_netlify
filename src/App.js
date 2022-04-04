8978e0770dbef515be5491fc72fb7fdd44b2b04b
Simon <snsimonescobar@gmail.com>
2022-04-03 19:39:36 -0500

    cambio de texto

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Esto es una prueba para deploy
+          Esto es una prueba para deploy, y hay un cambio nuevo.
         </p>
       </header>
     </div>
